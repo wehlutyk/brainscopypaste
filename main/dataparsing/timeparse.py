186d9b5bcc462eadd69dd8049ee21498333abb3f
Sébastien Lerique <sl@mehho.net>
2012-03-05 17:51:47 +0100

    Added a parsing function for ISO 8601 time stamps with time zones

---
+++ main/dataparsing/timeparse.py
@@ -0,0 +1,38 @@
+from datetime import timedelta, datetime
+from calendar import timegm
+from pylab import sign
+import re
+
+def isostr_to_epoch(isostr):
+    '''
+    Takes a string in format '%Y-%m-%dT%H:%M:%SZ'
+                          or '%Y-%m-%dT%H:%M:%S+%Hz:%Mz'
+                          or '%Y-%m-%dT%H:%M:%S-%Hz:%Mz'
+    and converts it to a number of seconds since the epoch
+    '''
+    
+    # Convert the 'Z' to '+00:00'
+    tzisostr = re.sub(r'Z$', '+00:00', isostr)
+    
+    # Get the timezone offset and strip it from the tzisostr
+    try:
+        if len(tzisostr) >= 20:
+            tzoffset = int(tzisostr[-6:-3])
+            offsetstr = tzisostr[:-6]
+        else:
+            tzoffset = 0
+            offsetstr = tzisostr
+    except:
+        print 'Exception caught in int()'
+        print 'isostr =', isostr
+    
+    # Convert the offsetstr to a datetime
+    DT = datetime(2000, 1, 1)
+    offsetdt = datetime.strptime(offsetstr, '%Y-%m-%dT%H:%M:%S')
+    
+    # Correct the offsetdt by the tzoffset
+    utcdt = offsetdt - sign(tzoffset)*timedelta(hours=tzoffset)
+    assert utcdt.tzinfo is None
+    
+    # Finally, return the number of seconds since epoch
+    return timegm(utcdt.utctimetuple())
