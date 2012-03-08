eb395b2afa4d32fcd57bd288b2019af9c7da8dc3
Sébastien Lerique <sl@mehho.net>
2012-03-08 18:10:07 +0100

    Updated datainterfaces init script

--- datainterfaces/__init__.py
+++ datainterfaces/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__all__ = ['nltktools', 'spinn3r', 'timeparsing', 'xmlparsing']
+__all__ = ['memetracker', 'nltktools', 'spinn3r', 'timeparsing', 'xmlparsing']
