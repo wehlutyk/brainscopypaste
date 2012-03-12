4e20cb255a4566dd18b4eb7ff215e7d093ee7eda
Sébastien Lerique <sl@mehho.net>
2012-03-12 18:34:05 +0100

    Corrected an encoding bug in picklesaver.py

--- datainterfaces/picklesaver.py
+++ datainterfaces/picklesaver.py
@@ -7,18 +7,17 @@ Methods for loading and saving data structures, using pickle
 
 # Imports
 import pickle
-from codecs import open as c_open
 
 
 # Module code
 def save(s, filepath):
     ''' Save a structure '''
-    with c_open(filepath, 'wb', encoding='utf-8') as f:
+    with open(filepath, 'wb') as f:
         pickle.dump(s, f)
 
 
 def load(filepath):
     ''' Load a structure '''
-    with c_open(filepath, 'rb', encoding='utf-8') as f:
+    with open(filepath, 'rb') as f:
         s = pickle.load(f)
     return s
