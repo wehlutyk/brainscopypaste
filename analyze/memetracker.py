3166725ab52990070b3cbb021f15c6d4223c61f4
Sébastien Lerique <sl@mehho.net>
2012-03-14 15:42:18 +0100

    Refactored analyze/memetracker.py a little, with better functionality separation.

--- analyze/memetracker.py
+++ analyze/memetracker.py
@@ -12,7 +12,7 @@ import numpy as np
 
 
 # Module code
-def frame_cluster(cluster):
+def frame_cluster_5day(cluster):
     '''
     Create a new cluster by cropping the one passed as an argument in a +/-48h time frame
     around the 24h with highest activity
@@ -28,15 +28,19 @@ def frame_cluster(cluster):
     start = max_24h - span_before
     end = max_24h + 86400 + span_after
     
+    return frame_cluster(cluster, start, end)
+
+
+def frame_cluster(cl, start, end):
     framed_quotes = {}
-    for qt in cluster.quotes.values():
+    for qt in cl.quotes.values():
         qt.compute_attrs()
         if (start <= qt.start <= end) or (qt.start <= start <= qt.end):
             framed_quotes[qt.id] = frame_quote(qt, start, end)
     
     n_quotes = len(framed_quotes)
     tot_freq = sum([qt.tot_freq for qt in framed_quotes.values()])
-    framed_cluster = d_mt.Cluster(n_quotes=n_quotes, tot_freq=tot_freq, root=cluster.root, cl_id=cluster.id)
+    framed_cluster = d_mt.Cluster(n_quotes=n_quotes, tot_freq=tot_freq, root=cl.root, cl_id=cl.id)
     framed_cluster.quotes = framed_quotes
     
     return framed_cluster
@@ -47,15 +51,18 @@ def frame_quote(qt, start, end):
     Create a new quote from the one passed as an argument, but framed in the time-window [start, end]
     '''
     
-    times = qt.url_times
-    framed_times = times[np.where((start <= times) * (times <= end))]
+    framed_times = frame_timeline(qt, start, end)
     n_urls = len(set(framed_times))
     tot_freq = len(framed_times)
     framed_qt = d_mt.Quote(n_urls=n_urls, tot_freq=tot_freq, string=qt.string, qt_id=qt.id)
-    framed_qt.url_times = framed_times.copy()
+    framed_qt.url_times = framed_times
     framed_qt.current_idx = tot_freq
     framed_qt.compute_attrs()
     return framed_qt
+
+
+def frame_timeline(tm, start, end):
+    return tm.url_times[np.where((start <= tm.url_times) * (tm.url_times <= end))].copy()
 
 
 def find_max_24h_frame(timeline):
@@ -77,3 +84,15 @@ def find_max_24h_frame(timeline):
         ipd_all[i] = np.histogram(timeline.url_times, 1, (st, st+86400))[0][0]
     
     return start_times[np.argmax(ipd_all)]
+
+
+#def get_timebags(cluster, n_bags):
+#    '''
+#    Return n_bags bags of quotes computed from the cluster (time bags)
+#    '''
+#    pass
+#
+#
+#class TimeBag(object):
+#    def __init__(self, cluster, start, end):
+#        pass
