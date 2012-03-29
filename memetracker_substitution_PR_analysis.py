cc31bff82a598bad849fc8d09c1b49819441edb8
Sébastien Lerique <sl@mehho.net>
2012-03-29 19:22:03 -0300

    Modified memetracker_substitution_PR_analysis.py to compare only synonyms.

--- memetracker_substitution_PR_analysis.py
+++ memetracker_substitution_PR_analysis.py
@@ -7,12 +7,12 @@ This script fetches the PR scores of words that are substituted through time in 
 It takes each Cluster, splits it into a number of TimeBags, then looks at the transitions between TimeBags
 defined by the 'bag_transitions' variable: for each transition (e.g. TimeBag #0 to TimeBag #2), it takes
 the highest frequency string in the first TimeBag, gets all strings in the second TimeBag which are at
-hamming_word-distance == 1, and stores the scores of the substituted word and the new word. If any one of
-those words is not referenced in the PR scores, it takes note of it (in the 'nonlemmas' variable). It then
-saves all the data to pickle files. The output is two files: one containing the ranks of (substituted word, new word)
-couples (which is a Nx2 numpy array), the other containing a list of dicts with data about the words that didn't
-have PR scores (data stored is: cluster id ('cl_id'), tokenized start string ('t_smax'), tokenized end string ('t_s')
-and index of the changed word ('idx')).
+hamming_word-distance == 1, and stores the scores of the substituted word and the new word if they happen to be
+synonyms. If any one of those words is not referenced in the PR scores, it takes note of it (in the 'nonlemmas'
+variable). It then saves all the data to pickle files. The output is two files: one containing the ranks of
+(substituted word, new word) couples (which is a Nx2 numpy array), the other containing a list of dicts with
+data about the words that didn't have PR scores (data stored is: cluster id ('cl_id'), tokenized start string
+('t_smax'), tokenized end string ('t_s') and index of the changed word ('idx')).
 
 """
 
@@ -20,15 +20,16 @@ and index of the changed word ('idx')).
 # Imports
 from __future__ import division
 from nltk import word_tokenize
-import numpy as np
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet as wn
+import numpy as np
 import datainterface.picklesaver as ps
 import settings as st
 
 
 # Code
-n_timebags = 3
-bag_transitions = [(1, 2)]
+n_timebags = 2
+bag_transitions = [(0, 1)]
 sphere_radius = 1
 pickle_transitionranks = st.memetracker_PR_transitionranks_pickle
 pickle_nonlemmas = st.memetracker_PR_nonlemmas_pickle
@@ -75,19 +76,21 @@ for cl in clusters.itervalues():
             t_s = word_tokenize(s)
             t_smax = word_tokenize(smax)
             idx = np.where([w1 != w2 for (w1, w2) in zip(t_s, t_smax)])[0]
-#            # Lemmatize the words
-#            lem1 = lemmatizer.lemmatize(t_smax[idx])
-#            lem2 = lemmatizer.lemmatize(t_s[idx])
-            # Don't lemmatize words
-            lem1 = t_smax[idx]
-            lem2 = t_s[idx]
-            # See if the concerned words are in the PR scores
-            try:
-                # If so, store the two PR ranks
-                transitionranks.append([ PR[lem1], PR[lem2] ])
-            except KeyError:
-                # If not, keep track of what we left out
-                nonlemmas.append({'cl_id': cl.id, 't_s': t_s, 't_smax': t_smax, 'idx': idx})
+            # Lemmatize the words
+            lem1 = lemmatizer.lemmatize(t_smax[idx])
+            lem2 = lemmatizer.lemmatize(t_s[idx])
+#            # Don't lemmatize words
+#            lem1 = t_smax[idx]
+#            lem2 = t_s[idx]
+            # See if the words have a common synset, i.e. are they synonyms
+            if len(set(wn.synsets(lem1)).intersection(set(wn.synsets(lem2)))) > 0:
+                # See if the concerned words are in the PR scores
+                try:
+                    # If so, store the two PR ranks
+                    transitionranks.append([ PR[lem1], PR[lem2] ])
+                except KeyError:
+                    # If not, keep track of what we left out
+                    nonlemmas.append({'cl_id': cl.id, 't_s': t_s, 't_smax': t_smax, 'idx': idx})
 
 
 # And save the data
