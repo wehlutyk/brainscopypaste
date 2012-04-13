ffd748b0dda89a29e8a917b7a283411d60dec5dc
Sébastien Lerique <sl@mehho.net>
2012-04-13 19:57:01 -0300

    Conformed the analyze package to PEP 8 (Python code style guide).

--- analyze/linalg.py
+++ analyze/linalg.py
@@ -4,37 +4,48 @@
 """Linear algebra tools, specifically to compute the PageRank algorithm.
 
 Methods:
-  * matrix_normalize_columns: Normalize a CSC or CSR matrix on its columns, and return in
-                              'outfmt' format ('csc' or 'csr')
+  * matrix_normalize_columns: Normalize a CSC or CSR matrix on its columns
   * norm_l1: compute the l1 norm of an array
-  * matrix_eigen_solve: solve the v = M*v eigenproblem by the Power Iteration algorithm
+  * matrix_eigen_solve: solve the v = M*v eigenproblem by the Power Iteration
+                        algorithm
 
 """
 
 
-# Imports
 from __future__ import division
+
 import numpy as np
 from scipy import random
 
 
-# Module code
 def matrix_normalize_columns(M, outfmt):
-    """Normalize a CSC or CSR matrix on its columns, and return in 'outfmt' format ('csc' or 'csr')."""
+    """Normalize a CSC or CSR matrix on its columns.
+    
+    Arguments:
+      * outfmt: the output format for the normalized matrix. Accepted values
+                are 'csc' and 'csr'.
+    
+    """
     
     # If the matrix is CSR, convert to CSC
+    
     if M.getformat() == 'csr':
         M = M.tocsc()
     
     # Normalize on the columns
+    
     print 'Normalizing matrix on the columns...',
     num_lems = M.get_shape()[0]
+    
     for i in xrange(num_lems):
+        
         col_vals = M.data[M.indptr[i]:M.indptr[i+1]].copy()
         M.data[M.indptr[i]:M.indptr[i+1]] = col_vals / np.sum(col_vals)
+    
     print 'OK'
     
     # Convert to outfmt if necessary
+    
     if outfmt == 'csr':
         M = M.tocsr()
     elif outfmt != 'csc':
@@ -51,9 +62,10 @@ def norm_l1(v):
 def matrix_eigen_solve(M, v0, max_it, tol):
     """Solve the v = M*v eigenproblem by the Power Iteration algorithm.
     
-    This will only work on a stochastic matrix, since there is no vector renormalization
-    (it created precision problems). Note that to avoid oscillation in the algorithm, each
-    iteration consists in fact of a random number (1-9) of iterations.
+    This will only work on a stochastic matrix, since there is no vector
+    renormalization (it creates precision problems). Note that to avoid
+    oscillation in the algorithm, each iteration consists in fact of a random
+    number (1-9) of iterations.
     
     Arguments:
       * M: the matrix for the eigenproblem, preferably in CSR format
@@ -69,22 +81,29 @@ def matrix_eigen_solve(M, v0, max_it, tol):
     """
     
     # Initialize
+    
     v = v0.copy()
     vv = M.dot(M.dot(v))
     nit = 0
     
     # Loop until we reach the tolerance
+    
     while norm_l1(vv - v) >= tol:
         
         # Do the iteration calculus
+        
         v = vv
+        
         for i in range(random.randint(1, 10)):
             vv = M.dot(M.dot(vv))
         
         # Or until we do too many iterations
+        
         nit += 1
+        
         if nit >= max_it:
             break
     
-    # Return the solution, along with convergence information
+    # Return convergence information with the solution
+    
     return (vv, nit, norm_l1(vv - v))
--- analyze/memetracker.py
+++ analyze/memetracker.py
@@ -4,41 +4,51 @@
 """Analyze data from the MemeTracker dataset.
 
 Methods:
-  * frame_cluster_around_peak: cut off quote occurrences in a Cluster around the 24h
-                               window with maximum activity
-  * frame_cluster: cut off quote occurrences in a Cluster at the specified boundaries
-  * frame_quote: cut off quote occurrences in a Quote at the specified boundaries
-  * frame_timeline: cut off quote occurrences in a Timeline at the specified boundaries
+  * frame_cluster_around_peak: cut off quote occurrences in a Cluster around
+                               the 24h window with maximum activity
+  * frame_cluster: cut off quote occurrences in a Cluster at the specified
+                   boundaries
+  * frame_quote: cut off quote occurrences in a Quote at the specified
+                 boundaries
+  * frame_timeline: cut off quote occurrences in a Timeline at the specified
+                    boundaries
   * find_max_24h_window: find the 24h window of maximum activity in a Timeline
   * build_timebags: build a number of TimeBags from a Cluster
   * build_n_quotes_to_clusterids: build a dict associating number of Quotes to
                                   Cluster ids having that number of quotes
-  * build_quoteslengths_to_quoteids: build a dict associating Quote string lengths to
-                                     the number of Quotes having that string length
-  * _build_timebag_transitions: recursively build the list of possible transitions from a number
-                                of TimeBags (private method, used by 'build_timebag_transitions')
-  * build_timebag_transitions: build the list of possible transitions from a number of TimeBags
+  * build_quoteslengths_to_quoteids: build a dict associating Quote string
+                                     lengths to the number of Quotes having
+                                     that string length
+  * _build_timebag_transitions: recursively build the list of possible
+                                transitions from a number of TimeBags (private
+                                method, used by 'build_timebag_transitions')
+  * build_timebag_transitions: build the list of possible transitions from a
+                               number of TimeBags
 
 """
 
 
-# Imports
 from __future__ import division
+
 import numpy as np
 from nltk import word_tokenize
+
 import datastructure.memetracker as ds_mt
 
 
-# Module code
-def frame_cluster_around_peak(cl, span_before=2*86400, span_after=2*86400):
-    """Cut off quote occurrences in a Cluster around the 24h window with maximum activity.
+def frame_cluster_around_peak(cl, span_before=2 * 86400,
+                              span_after=2 * 86400):
+    """Cut off quote occurrences in a Cluster around the 24h window with
+    maximum activity.
     
     Arguments:
       * cl: the Cluster to work on
     
     Optional arguments:
-      * span_before: time span (in seconds) to include before the beginning of the max 24h window; defaults to 2 days
-      * span_after: time span (in seconds) to include after the end of the max 24h window; defautls to 2 days
+      * span_before: time span (in seconds) to include before the beginning of
+                     the max 24h window. Defaults to 2 days.
+      * span_after: time span (in seconds) to include after the end of the max
+                    24h window. Defaults to 2 days.
     
     Returns: a new framed Cluster.
     
@@ -58,30 +68,41 @@ def frame_cluster(cl, start, end):
     
     Arguments:
       * cl: the Cluster to work on
-      * start: time (in seconds from epoch) of the beginning of the target time window
+      * start: time (in seconds from epoch) of the beginning of the target
+               time window
       * end: time (in seconds from epoch) of the end of the target time window
     
     Returns: a new framed Cluster.
     
     """
     
-    # Recreate each Quote, framed
     framed_quotes = {}
+    
     for qt in cl.quotes.values():
-        # Compute the starting time, ending time, time span, etc
+        
+        # Compute the starting time, ending time, time span, etc.
+        
         qt.compute_attrs()
-        # If the Quote intersects with the requested time window, then include it
+        
+        # If the Quote intersects with the requested time window, include it.
+        
         if (start <= qt.start <= end) or (qt.start <= start <= qt.end):
+            
             framed_quote = frame_quote(qt, start, end)
-            # This check is in case a Quote starts before 'start', ends after 'end', but has no
-            # occurrences between 'start' and 'end' (in which case 'framed_quote' is empty)
+            
+            # If the Quote starts before 'start', ends after 'end', but has no
+            # occurrences between 'start' and 'end' (in which case
+            # 'framed_quote' is empty), exclude it.
+            
             if framed_quote != None:
                 framed_quotes[qt.id] = framed_quote
     
-    # Create the new framed Cluster
+    # Create the new framed Cluster.
+    
     n_quotes = len(framed_quotes)
     tot_freq = sum([qt.tot_freq for qt in framed_quotes.values()])
-    framed_cluster = ds_mt.Cluster(n_quotes=n_quotes, tot_freq=tot_freq, root=cl.root, cl_id=cl.id)
+    framed_cluster = ds_mt.Cluster(n_quotes=n_quotes, tot_freq=tot_freq,
+                                   root=cl.root, cl_id=cl.id)
     framed_cluster.quotes = framed_quotes
     
     return framed_cluster
@@ -92,26 +113,34 @@ def frame_quote(qt, start, end):
     
     Arguments:
       * qt: the Quote to work on
-      * start: time (in seconds from epoch) of the beginning of the target time window
+      * start: time (in seconds from epoch) of the beginning of the target
+               time window
       * end: time (in seconds from epoch) of the end of the target time window
     
     Returns: a new framed Quote.
     
     """
     
-    # Frame the Timeline of the Quote
+    # Frame the Timeline of the Quote.
+    
     framed_times = frame_timeline(qt, start, end)
-    # Check its not empty
+    
+    # Check its not empty.
+    
     if len(framed_times) == 0:
         return None
-    # Then create the new framed Quote
+    
+    # Then create the new framed Quote.
+    
     n_urls = len(set(framed_times))
     tot_freq = len(framed_times)
-    framed_qt = ds_mt.Quote(n_urls=n_urls, tot_freq=tot_freq, string=qt.string, qt_id=qt.id)
+    framed_qt = ds_mt.Quote(n_urls=n_urls, tot_freq=tot_freq,
+                            string=qt.string, qt_id=qt.id)
     framed_qt.url_times = framed_times
     framed_qt.current_idx = tot_freq
     
-    # And compute its attributes
+    # And compute its attributes.
+    
     framed_qt.compute_attrs()
     
     return framed_qt
@@ -122,50 +151,63 @@ def frame_timeline(tm, start, end):
     
     Arguments:
       * tm: the Timeline to work on
-      * start: time (in seconds from epoch) of the beginning of the target time window
+      * start: time (in seconds from epoch) of the beginning of the target
+               time window
       * end: time (in seconds from epoch) of the end of the target time window
     
     Returns: a new framed Timeline.
     
     """
     
-    # Careful to return a copy, otherwise we just get a particular view of the same memory space,
-    # which is bad for further modifications
-    return tm.url_times[np.where((start <= tm.url_times) * (tm.url_times <= end))].copy()
+    # Careful to return a copy, otherwise we just get a particular view of
+    # the same memory space, which is bad for further modifications.
+    
+    return tm.url_times[np.where((start <= tm.url_times) *
+                                 (tm.url_times <= end))].copy()
 
 
-def find_max_24h_window(timeline, prec=30*60):
+def find_max_24h_window(timeline, prec=30 * 60):
     """Find the 24h window of maximum activity in a Timeline.
     
     Arguments:
       * timeline: the Timeline to scan
     
     Optional arguments:
-      * prec: the precision (in seconds) of the position of the returned time window. Defaults to half an hour.
+      * prec: the precision (in seconds) of the position of the returned time
+              window. Defaults to half an hour.
     
-    Returns: the time (in seconds from epoch) of the beginning of the maximum activity window.
+    Returns: the time (in seconds from epoch) of the beginning of the maximum
+             activity window.
     
     """
     
-    # How many windows are we testing
-    n_windows = int(np.ceil(2*86400/prec))
+    # How many windows are we testing.
     
-    # Compute the Timeline attributes
-    if not timeline.attrs_computed:
-        timeline.compute_attrs()
+    n_windows = int(np.ceil(2 * 86400 / prec))
     
-    # Get the first estimation of where the maximum is;
-    # it has a precision of 1 day (see details of Timeline.compute_attrs())
+    # Compute the Timeline attributes.
+    
+    timeline.compute_attrs()
+    
+    # First estimation of where the maximum is; it has a precision of 1 day
+    # (see details of Timeline.compute_attrs()).
+    
     base_time = timeline.max_ipd_x_secs - 86400
-    # Set the starting times of the time windows we're testing
-    start_times = np.arange(n_windows)*prec + base_time
     
-    # Compute activity for each time window
+    # Starting times of the time windows we're testing.
+    
+    start_times = np.arange(n_windows) * prec + base_time
+    
+    # Compute activity for each time window.
+    
     ipd_all = np.zeros(n_windows)
-    for i, st in enumerate(start_times):
-        ipd_all[i] = np.histogram(timeline.url_times, 1, (st, st+86400))[0][0]
     
-    # And get the max
+    for i, st in enumerate(start_times):
+        ipd_all[i] = np.histogram(timeline.url_times, 1,
+                                  (st, st + 86400))[0][0]
+    
+    # And get the max.
+    
     return start_times[np.argmax(ipd_all)]
 
 
@@ -180,21 +222,27 @@ def build_timebags(cluster, n_bags):
     
     """
     
-    # Build the Timeline for the Cluster, set the parameters for the TimeBags
+    # Build the Timeline for the Cluster, set the parameters for the TimeBags.
+    
     cluster.build_timeline()
+    
     step = int(round(cluster.timeline.span.total_seconds() / n_bags))
     cl_start = cluster.timeline.start
     
-    # Create the sequence of TimeBags
+    # Create the sequence of TimeBags.
+    
     timebags = []
+    
     for i in xrange(n_bags):
-        timebags.append(ds_mt.TimeBag(cluster, cl_start + i*step, cl_start + (i+1)*step))
+        timebags.append(ds_mt.TimeBag(cluster, cl_start + i * step,
+                                      cl_start + (i + 1) * step))
     
     return timebags
 
 
 def build_n_quotes_to_clusterids(clusters):
-    """Build a dictionary associating number of Quotes to Cluster ids having that number of quotes.
+    """Build a dictionary associating number of Quotes to Cluster ids having
+    that number of quotes.
     
     Arguments:
       * The dict of Clusters to work on
@@ -203,9 +251,10 @@ def build_n_quotes_to_clusterids(clusters):
     
     """
     
-    # Self-explanatory
     inv_cl_lengths = {}
+    
     for cl in clusters.values():
+        
         if inv_cl_lengths.has_key(cl.n_quotes):
             inv_cl_lengths[cl.n_quotes].append(cl.id)
         else:
@@ -215,20 +264,25 @@ def build_n_quotes_to_clusterids(clusters):
 
 
 def build_quotelengths_to_n_quote(clusters):
-    """Build a dict associating Quote string lengths to the number of Quotes having that string length.
+    """Build a dict associating Quote string lengths to the number of Quotes
+    having that string length.
     
     Arguments:
       * The dict of Clusters to work on
     
-    Returns: the dict of 'Quote string lengths' -> 'number of Quotes having that string length'.
+    Returns: the dict of 'Quote string lengths' -> 'number of Quotes having
+             that string length'.
     
     """
     
-    # Self-explanatory
     inv_qt_lengths = {}
+    
     for cl in clusters.values():
+        
         for qt in cl.quotes.values():
+            
             n_words = len(word_tokenize(qt.string.lower()))
+            
             if inv_qt_lengths.has_key(n_words):
                 inv_qt_lengths[n_words] += 1
             else:
@@ -238,20 +292,26 @@ def build_quotelengths_to_n_quote(clusters):
 
 
 def _build_timebag_transitions(bag_indices, transitions):
-    """Recursively build the list of possible transitions from a number of TimeBags.
+    """Recursively build the list of possible transitions from a number of
+    TimeBags.
     
     Arguments:
-      * bag_indices: the indices of the TimeBags between which to build transitions
-      * transitions: the list passed on to the recursive instances of the method, containing what
-                     transitions have already been generated
+      * bag_indices: the indices of the TimeBags between which to build
+                     transitions
+      * transitions: the list passed on to the recursive instances of the
+                     method, containing what transitions have already been
+                     generated
     
-    Returns: a list of tuples, each tuple representing a transition from one TimeBag to a later one.
+    Returns: a list of tuples, each tuple representing a transition from one
+             TimeBag to a later one.
     
     """
     
     if len(bag_indices) > 1:
-        transitions.extend([ (bag_indices[0], idx) for idx in bag_indices[1:] ])
+        
+        transitions.extend([(bag_indices[0], idx) for idx in bag_indices[1:]])
         _build_timebag_transitions(bag_indices[1:], transitions)
+        
     else:
         return []
 
@@ -264,10 +324,12 @@ def build_timebag_transitions(n_timebags):
     Arguments:
       * the number of TimeBags
     
-    Returns: a list of tuples, each tuple representing a transition from one TimeBag to a later one.
+    Returns: a list of tuples, each tuple representing a transition from one
+             TimeBag to a later one.
     
     """
     
     transitions = []
     _build_timebag_transitions(range(n_timebags), transitions)
+    
     return transitions
