c8d9aaad64a2b76f89ef2716d065e64f4659984f
Sébastien Lerique <sl@mehho.net>
2012-04-12 23:40:25 -0300

    * Changed the eigenproblem solver to a manual implementation * Modified the construction of the WN adjacency matrix to convert all lemmas to lowercase

--- linguistics/wordnettools.py
+++ linguistics/wordnettools.py
@@ -4,11 +4,13 @@
 """Compute PageRank scores and adjacency matrices corresponding to the Wordnet synonyms graph.
 
 Methods:
-  * build_lem_coords: build a dictionary associating each lemma in Wordnet to a coordinate
-  * build_wn_adjacency_matrix_Scipy: build the adjacency matrix (in Scipy Compressed Sparse Row
-                                     format) for the WN synonyms graph
-  * build_wn_PR_adjacency_matrix_PETSc: build the adjacency matrix (in PETSc Mat format)
-                                        for the WN synonyms graph, normalized as PageRank needs
+  * build_wn_coords: build a dictionary associating each lemma (in lowercase) in Wordnet to a coordinate
+  * build_wn_adjacency_matrix: build the adjacency matrix (in CSC or CSR format) for
+                               the WN synonyms graph
+  * matrix_normalize_columns: Normalize a CSC or CSR matrix on its columns, and return in
+                              'outfmt' format ('csc' or 'csr')
+  * norm_l1: compute the l1 norm of an array
+  * matrix_eigen_solve: solve the v = M*v eigenproblem by the Power Iteration algorithm
   * build_wn_PR_scores: compute the PageRank scores corresponding to the WN synonyms graph
   * build_wn_degrees: compute the degrees of lemmas in the WN graph (not excluding lemmas not
                       connected to other lemmas)
@@ -27,17 +29,16 @@ The PageRank scores computed depend on the following details:
 # Imports
 from __future__ import division
 from nltk.corpus import wordnet as wn
-from scipy.sparse import csr_matrix
+from scipy.sparse import csc_matrix, csr_matrix
 import numpy as np
-#`from petsc4py import PETSc' in `build_wn_PR_adjacency_matrix_PETSc'
-#`from slepc4py import SLEPc' in `build_wn_PR_scores'
+from scipy import random
 
 
 # Module code
-def build_lem_coords():
-    """Build a dictionary associating each lemma in Wordnet to a coordinate.
+def build_wn_coords():
+    """Build a dictionary associating each lemma (in lowercase) in Wordnet to a coordinate.
     
-    Returns: a dict which keys are the lemmas in Wordnet, and the value for a key is
+    Returns: a dict which keys are the lemmas (in lowercase) in Wordnet, and the value for a key is
              the coordinate for that lemma, to be used e.g. with an adjacency matrix.
     
     """
@@ -50,85 +51,117 @@ def build_lem_coords():
     for syn in wn.all_synsets():
         if len(syn.lemma_names) > 1:
             for lem in syn.lemma_names:
-                if not lem_coords.has_key(lem):
-                    lem_coords[lem] = i
+                if not lem_coords.has_key(lem.lower()):
+                    lem_coords[lem.lower()] = i
                     i += 1
     print 'OK'
     
     return lem_coords
     
 
-def build_wn_adjacency_matrix_Scipy(lem_coords):
-    """Build the adjacency matrix (in Compressed Sparse Row format) for the WN synonyms graph.
+def build_wn_adjacency_matrix(lem_coords, outfmt):
+    """Build the adjacency matrix (in CSC or CSR format) for the WN synonyms graph.
     
-    Returns: the adjacency matrix, in Scipy CSR format, of the WN synonyms graph,
+    Arguments:
+      * lem_coords: the dict of lemma coordinates in the matrix, as return by 'build_wn_coords'
+      * outfmt: a string specifying the compression format for the result matrix ; can be 'csc' or 'csr'.
+    
+    Returns: the adjacency matrix, in Scipy CSC or CSR format, of the WN synonyms graph,
              with zeros on the diagonal, omitting lemmas which are not connected to any other
              (i.e. omitting lemmas that are alone in their synset)
     
     """
     
-    # Build the Compressed Sparse Row matrix corresponding to the synonyms network in Wordnet
-    print 'Building Wordnet adjacency matrix (Scipy CSR)...',
+    # Build the Compressed Sparse Column/Row matrix corresponding to the synonyms network in Wordnet
+    print 'Building Wordnet adjacency matrix...',
     # This first block of code should result in a symmetric matrix with zeros on the diagonal
     ij = ([], [])
     for syn in wn.all_synsets():
         if len(syn.lemma_names) > 1:
             for lem1 in syn.lemma_names:
                 for lem2 in syn.lemma_names:
-                    if lem1 != lem2:
-                        ij[0].append(lem_coords[lem1])
-                        ij[1].append(lem_coords[lem2])
+                    if lem1.lower() != lem2.lower():
+                        ij[0].append(lem_coords[lem1.lower()])
+                        ij[1].append(lem_coords[lem2.lower()])
     
-    # Create the Scipy CSR matrix
+    # Create the Scipy CSC/CSR matrix
     num_lems = len(lem_coords)
-    tM_sp = csr_matrix((np.ones(len(ij[0])), ij), shape=(num_lems, num_lems), dtype=np.float)
+    build_matrix = {'csc': csc_matrix, 'csr': csr_matrix}
+    M = build_matrix[outfmt]((np.ones(len(ij[0])), ij), shape=(num_lems, num_lems), dtype=np.float)
     print 'OK'
     
-    return tM_sp
+    return M
 
 
-def build_wn_PR_adjacency_matrix_PETSc(lem_coords):
-    """Build the adjacency matrix (in PETSc Mat format) for the WN synonyms graph, normalized as PageRank needs.
+def matrix_normalize_columns(M, outfmt):
+    """Normalize a CSC or CSR matrix on its columns, and return in 'outfmt' format ('csc' or 'csr')."""
     
-    Returns: the adjacency matrix, in PETSc Matrix format, of the WN synonyms graph,
-             with zeros on the diagonal, omitting lemmas which are not connected to any other
-             (i.e. omitting lemmas that are alone in their synset), normalized on the columns
-             (as PageRank needs it)
+    # If the matrix is CSR, convert to CSC
+    if M.getformat() == 'csr':
+        M = M.tocsc()
+    
+    # Normalize on the columns
+    print 'Normalizing matrix on the columns...',
+    num_lems = M.get_shape()[0]
+    for i in xrange(num_lems):
+        col_vals = M.data[M.indptr[i]:M.indptr[i+1]].copy()
+        M.data[M.indptr[i]:M.indptr[i+1]] = col_vals / np.sum(col_vals)
+    print 'OK'
+    
+    # Convert to outfmt if necessary
+    if outfmt == 'csr':
+        M = M.tocsr()
+    elif outfmt != 'csc':
+        raise Exception("Unrecognized matrix format: '{}'".format(outfmt))
+    
+    return M
+
+
+def norm_l1(v):
+    """Compute the l1 norm of an array."""
+    return np.sum(np.abs(v))
+
+
+def matrix_eigen_solve(M, v0, max_it, tol):
+    """Solve the v = M*v eigenproblem by the Power Iteration algorithm.
+    
+    This will only work on a stochastic matrix, since there is no vector renormalization
+    (it created precision problems). Note that to avoid oscillation in the algorithm, each
+    iteration consists in fact of a random number (1-9) of iterations.
+    
+    Arguments:
+      * M: the matrix for the eigenproblem, preferably in CSR format
+      * v0: a starting vector for the iteration ; should be normalized
+      * max_it: the maximum number of iterations that will be performed
+      * tol: the relative precision requested
+    
+    Returns: a tuple consisting of:
+      * vv: the final eigenvector
+      * nit: the number of iterations completed
+      * prec: the final relative precision
     
     """
     
-    # Import PETSc here only, since it's a pain to install and isn't useful for other methods
-    # (that way you don't need to have it to run e.g. 'build_wn_degrees')
-    from petsc4py import PETSc
+    # Initialize
+    v = v0.copy()
+    vv = M.dot(M.dot(v))
+    nit = 0
     
-    # Build the Compressed Sparse Row matrix corresponding to the synonyms network in Wordnet
-    tM_sp = build_wn_adjacency_matrix_Scipy(lem_coords)
+    # Loop until we reach the tolerance
+    while norm_l1(vv - v) >= tol:
+        
+        # Do the iteration calculus
+        v = vv
+        for i in range(random.randint(1, 10)):
+            vv = M.dot(M.dot(vv))
+        
+        # Or until we do too many iterations
+        nit += 1
+        if nit >= max_it:
+            break
     
-    # Compensate the weights by dividing each value by the number of outward links from
-    # the corresponding lemma (see details of the PageRank algorithm)
-    # Here we divide by the row sum (which is the same as the column sum, since the matrix is
-    # symmetric), then transpose when converting to PETSc to change that into a division by column
-    # sum (which is what PageRank needs). This is because column slicing is inefficient in a CSR matrix.
-    # (The 't' in 'tM_sp' stands for transpose, because the result here needs to be transposed.)
-    print 'Compensating link weights with number of outlinks...',
-    num_lems = len(lem_coords)
-    for i in xrange(num_lems):
-        row_vals = tM_sp.data[tM_sp.indptr[i]:tM_sp.indptr[i+1]].copy()
-        tM_sp.data[tM_sp.indptr[i]:tM_sp.indptr[i+1]] = row_vals / np.sum(row_vals)
-    print 'OK'
-    
-    # Turn it into a PETSc CSR matrix, and transpose
-    print 'Converting matrix to a PETSc CSR matrix, and transposing...',
-    M_pet = PETSc.Mat().create()
-    M_pet.setSizes([num_lems, num_lems])
-    M_pet.setType(PETSc.Mat.Type.AIJ)
-    M_pet.setValuesCSR(tM_sp.indptr.copy(), tM_sp.indices.copy(), tM_sp.data.copy())
-    M_pet.assemble()
-    M_pet.transpose()
-    del tM_sp
-    print 'OK'
-    
-    return M_pet
+    # Return the solution, along with convergence information
+    return (vv, nit, norm_l1(vv - v))
 
 
 def build_wn_PR_scores():
@@ -139,91 +172,25 @@ def build_wn_PR_scores():
     
     """
     
-    # Import SLEPc here only, since it's a pain to install and isn't useful for other methods
-    # (that way you don't need to have it to run e.g. 'build_wn_degrees')
-    from slepc4py import SLEPc
-    
     # Build the lemma coordinates
-    lem_coords = build_lem_coords()
+    lem_coords = build_wn_coords()
+    num_lems = len(lem_coords)
     
-    # Get the normalized adjacency matrix in PETSc format
-    M_pet = build_wn_PR_adjacency_matrix_PETSc(lem_coords)
+    # Get the normalized adjacency matrix
+    M = matrix_normalize_columns(build_wn_adjacency_matrix(lem_coords, outfmt='csc'), outfmt='csr')
     
-    # Solve the eigenvalue problem
-    print 'Running SLEPc eigenproblem solver...',
-    E = SLEPc.EPS()
-    E.create()
-    E.setOperators(M_pet)
-    E.setProblemType(SLEPc.EPS.ProblemType.NHEP)
-    E.setDimensions(nev=3)
-    E.setTolerances(tol=1e-15, max_it=10000)
-    E.solve()
+    # Solve the eigenproblem
+    print 'Solving the eigenproblem...',
+    v0 = random.random(num_lems)
+    v0 = v0 / norm_l1(v0)
+    (scores, nit, prec) = matrix_eigen_solve(M, v0=v0, max_it=10000, tol=1e-10)
     print 'OK'
     
-    # Print some info about the solving process and results
+    # Some info on the results
     print
-    print "  ******************************"
-    print "  *** SLEPc Solution Results ***"
-    print "  ******************************"
-    print
-    
-    its = E.getIterationNumber()
-    print "  Number of iterations of the method: %d" % its
-    
-    eps_type = E.getType()
-    print "  Solution method: %s" % eps_type
-    
-    nev = E.getDimensions()[0]
-    print "  Number of requested eigenvalues: %d" % nev
-    
-    tol, maxit = E.getTolerances()
-    print "  Stopping condition: tol=%.4g, maxit=%d" % (tol, maxit)
-    
-    nconv = E.getConverged()
-    print "  Number of converged eigenpairs %d" % nconv
-    
-    # Print info about converged eigenpairs, and store the results
-    results = []
-    if nconv > 0:
-        print
-        print "          k          ||Ax-kx||/||kx|| "
-        print "  ----------------- ------------------"
-        for i in range(nconv):
-            # Create the results vectors
-            vr = M_pet.getVecRight()
-            vi = M_pet.getVecRight()
-            k = E.getEigenpair(i, vr, vi)
-            error = E.computeRelativeError(i)
-            results.append((k, vr, vi, error))
-            
-            if k.imag != 0.0:
-                print "   %9f%+9f j %12g" % (k.real, k.imag, error)
-            else:
-                print "   %12f    %12g" % (k.real, error)
-    else:
-        raise Exception('No eigenpairs converged in the SLEPc solver!')
-    
-    
-    # Now get the proper result
-    print
-    print 'Taking the first eigenvector with eigenvalue 1 to have only > 0 components...',
-    
-    found = False
-    for (k, vr, vi, error) in results:
-        if k.imag == 0.0 and round(k.real, 10) == 1.0:
-            scores = vr.getArray()
-            if np.prod(scores > 0) == True:
-                found = True
-                break
-            elif np.prod(scores < 0) == True:
-                found = True
-                scores = - scores
-                break
-    if not found:
-        raise Exception('No eigenvector was found for eigenvalue 1 with only > 0 components!')
-    
-    print 'OK'
-    print 'The final result has a relative error of %12g' % error
+    print '*** Results ***'
+    print '  Number of iterations:', nit
+    print '  Final precision:', prec
     
     # Plug back the scores into a dict of lemmas
     print 'Replugging the PageRank scores into a dict of lemmas...',
@@ -243,16 +210,16 @@ def build_wn_degrees():
     """
     
     # Build the lemma coordinates
-    lem_coords = build_lem_coords()
+    lem_coords = build_wn_coords()
     
-    # Get the WN adjacency matrix in Scipy CSR format
-    M_sp = build_wn_adjacency_matrix_Scipy(lem_coords)
+    # Get the WN adjacency matrix in Scipy CSC format
+    M = build_wn_adjacency_matrix(lem_coords, outfmt='csc')
     
     # Compute the degree of each lemma name
     print 'Computing the degree of each lemma...',
     lem_degrees = {}
     for (w, i) in lem_coords.iteritems():
-        lem_degrees[w] = sum(M_sp.data[M_sp.indptr[i]:M_sp.indptr[i+1]])
+        lem_degrees[w] = sum(M.data[M.indptr[i]:M.indptr[i+1]])
     print 'OK'
     
     return lem_degrees
