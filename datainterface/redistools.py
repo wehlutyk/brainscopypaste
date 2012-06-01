69948036a70b2d94f1e56d77ddf4bad8645c1a6a
Sébastien Lerique <sl@mehho.net>
2012-06-01 17:22:55 +0200

    Added possibility to save references to instancemethods in redis (same recipe as previous revision's pickle recipe).

--- datainterface/redistools.py
+++ datainterface/redistools.py
@@ -12,8 +12,33 @@ Classes:
 
 import re
 import cPickle
+import copy_reg
+import types
 
 import redis
+
+
+# Let us pickle instancemethods
+
+def _pickle_method(method):
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    return _unpickle_method, (func_name, obj, cls)
+
+
+def _unpickle_method(func_name, obj, cls):
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    return func.__get__(obj, cls)
+
+
+copy_reg.pickle(types.MethodType, _pickle_method, _unpickle_method)
 
 
 class PRedis(redis.Redis):
