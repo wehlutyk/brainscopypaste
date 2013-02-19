0fb801aa098bda0f70315f4e2813fd0eab66ebbb
Sébastien Lerique <seblerique@wanadoo.fr>
2013-02-19 19:18:48 +0100

    Add doc

--- datainterface/fs.py
+++ datainterface/fs.py
@@ -1,15 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Check for files and folders, build filenames and prefixes based on analysis
+parameters."""
+
+
 import os
 
 
 def check_folder(folder):
-    """Check if folder exists; if not, create it and notify the user."""
+    """Check if a folder exists or create it.
+
+    If the folder gets created (because absent), information is logged
+    (printed to stdout).
+
+    Parameters
+    ----------
+    folder : string
+        Path to the folder to check for.
+
+    """
+
     if not os.path.exists(folder):
         print "*** FS: '" + folder + "' does not exist. Creating it."
         os.makedirs(folder)
 
 
 def check_file(filename, for_read=False):
-    """Check if filename already exists; if it does, raise an exception."""
+    """Check if a file already exists.
+
+    Parameters
+    ----------
+    filename : string
+        Path to the file to check for.
+    for_read : bool, optional
+        If ``True``, raise an exception if the file is absent. If ``False``,
+        raise an exception if the file is present. Defaults to ``False``.
+
+    Returns
+    -------
+    res : bool
+        Always ``True`` (otherwise an exception will have been raised).
+
+    """
+
     if os.path.exists(filename):
 
         if not for_read:
@@ -25,6 +59,23 @@ def check_file(filename, for_read=False):
 
 
 def get_fileprefix(args):
+    """Create a file prefix based on analysis parameters.
+
+    File prefixes are used for saving and loading data to and from files for
+    different argument sets. This function implements the defined way of
+    naming files according to the argument set they stem from.
+
+    Parameters
+    ----------
+    args : :class:`~baseargs.BaseArgs`
+        The analysis parameters for which to create a file prefix.
+
+    Returns
+    -------
+    file_prefix : string
+        The prefix corresponding to the given `args`.
+
+    """
 
     # Create the file prefix according to 'args'.
 
@@ -41,6 +92,23 @@ def get_fileprefix(args):
 
 
 def get_filename(args):
+    """Create a full mining data filename base on analysis parameters.
+
+    Parameters
+    ----------
+    args : :class:`~baseargs.BaseArgs`
+        The analysis Parameters for which to create a filename.
+
+    Returns
+    -------
+    filename : string
+        The full filename to store data for the given mining args.
+
+    See Also
+    --------
+    get_fileprefix
+
+    """
 
     # Prevent circular imports
     import settings as st
