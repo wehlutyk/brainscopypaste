3751319a18377fae4589cbe34d56b3aef99b6f76
Sébastien Lerique <seblerique@wanadoo.fr>
2013-02-21 15:58:30 +0100

    Added requirements for ReadTheDocs, and module mocking

--- docs/conf.py
+++ docs/conf.py
@@ -255,3 +255,30 @@ todo_include_todos = True
 
 # Default flags for autodoc
 autodoc_default_flags = ['members', 'private-members']
+
+# -- Mock modules for ReadTheDocs ----------------------------------------------
+
+import sys
+
+class Mock(object):
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['numpy', 'pylab', 'scipy', 'matplolib', 'networkx', 'nltk', 'redis']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
