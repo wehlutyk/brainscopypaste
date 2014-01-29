70cc3687e005a9e61dae2c738f5219062d2a662a
Sébastien Lerique <sl@mehho.net>
2014-01-29 20:27:29 +0100

    Add autosummary extension to sphinx (solving a compile error)

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ for mod_name in MOCK_MODULES:
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'numpydoc', 'sphinx.ext.todo',
               'sphinx.ext.coverage', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode',
-              'sphinx.ext.intersphinx']
+              'sphinx.ext.intersphinx', 'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
