8c11497e25f0e770d5e4c55eb239134f7e192fc9
Sébastien Lerique <sl@mehho.net>
2014-01-30 16:11:54 +0100

    Document analyze/base.py

--- analyze/base.py
+++ analyze/base.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Base analysis objects upon which analysis classes build.
+
+See example usages in :mod:`analyze.features`, :mod:`analyze.paths`,
+and :mod:`analyze.positions`.
+
+"""
+
+
 from __future__ import division
 
 from warnings import warn
@@ -8,11 +19,67 @@ from datainterface.fs import check_file
 
 class AnalysisCase(object):
 
+    """Base analysis class on which you build an analysis case.
+
+    This class defines the interface that an analysis case should implement,
+    interface which is later used in :mod:`analyze.substitutions`.
+
+    Parameters
+    ----------
+    aa : :class:`AnalysisArgs` instance
+        Arguments for the analysis to be run.
+    data : any
+        The data to be analyzed by the `analyze` and `analyze_inner` methods.
+
+    Attributes
+    ----------
+    aa : :class:`AnalysisArgs` instance
+        The arguments passed to the constructor.
+    data : any
+        The data passed to the constructor.
+
+    """
+
     def __init__(self, aa, data):
+        """Initialize the structure with analysis arguments and analysis data.
+
+        Parameters
+        ----------
+        aa : :class:`AnalysisArgs` instance
+            Arguments for the analysis to be run.
+        data : any
+            The data to be analyzed by the `analyze` and `analyze_inner`
+            methods.
+
+        """
+
         self.aa = aa
         self.data = data
 
     def analyze(self, axs, filepath):
+        """Check for the existence of our target saving file, and run our
+        analysis.
+
+        If the target file already exists and `self.aa.save` is ``True``
+        (meaning the final figure should be saved), and `self.aa.overwrite`
+        is not, the analysis is aborted to not overwrite an existing figure.
+
+        Parameters
+        ----------
+        axs : list of :class:`matplotlib.axes.Axes` instance
+            The list of axes on which to plot the analysis.
+        filepath : string
+            The filepath to which the figure is saved if `self.aa.save`
+            is ``True``.
+
+        Returns
+        -------
+        bool
+            ``True`` if the analysis was performed, ``False`` if it was aborted
+            because of file existence.
+
+        """
+
         if self.aa.save and not self.checkfile(filepath):
             return False
 
@@ -20,18 +87,53 @@ class AnalysisCase(object):
         return True
 
     def build_axes(self):
+        """Return the (created) list of :class:`matplotlib.axes.Axes` on which
+        to plot the analysis (abstract method)."""
+
         raise NotImplementedError
 
     def analyze_inner(self, axs):
+        """Perform the analysis and plotting itself (abstract method).
+
+        Parameters
+        ----------
+        axs : list of :class:`matplotlib.axes.Axes` instance
+            The list of axes on which to plot the analysis.
+
+        """
+
         raise NotImplementedError
 
     def savefile_postfix(self):
+        """Return the filename postfix, used to create the save file
+        (abstract method)."""
+
         raise NotImplementedError
 
     def print_fig_text(self, fig, title):
+        """Print the `title` text on figure `fig`, possibly with additional
+        information (abstract method)."""
+
         raise NotImplementedError
 
     def checkfile(self, filepath):
+        """Check for existence of `filepath` and report depending on our
+        saving arguments.
+
+        Parameters
+        ----------
+        filepath : string
+            The file path to check for.
+
+        Returns
+        -------
+        bool
+            ``True`` if the analysis can take place (i.e. if either `filepath`
+            does not exist, or it does but `self.aa.overwrite` is ``True``),
+            ``False`` otherwise.
+
+        """
+
         try:
             check_file(filepath)
         except Exception:
@@ -46,6 +148,16 @@ class AnalysisCase(object):
 
     @classmethod
     def latexize(cls, text):
+        """Convert `text` (to be printed on a figure) to a more latex-friendly
+        format.
+
+        Returns
+        -------
+        string
+            The converted text.
+
+        """
+
         latex_text = re.sub('\\|', '$|$', text)
         latex_text = re.sub('_', ' ', latex_text)
         return latex_text
