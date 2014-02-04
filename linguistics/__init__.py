d4aa070e6ab7f9ad1828ce50751a3eb30a68010c
Sébastien Lerique <sl@mehho.net>
2014-02-04 17:19:55 +0100

    Check pep8 and fix doc for linguistics/__init__, cmu and distance

--- linguistics/__init__.py
+++ linguistics/__init__.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Linguistically analyze data
+"""Linguistically analyze data.
 
 These modules are used to do linguistic analysis of the data. They plug into
 the datastructure objects to provide an object-oriented interface for analysis
 of the data.
-
-Modules:
-  * lang_detection: detect the language in a text
-  * memetracker: linguistic analysis tools for the MemeTracker dataset
-  * wordnettools: compute PageRank scores and adjacency matrices corresponding
-                  to the Wordnet synonyms graph. Other tools with it.
 
 """
--- linguistics/cmu.py
+++ linguistics/cmu.py
@@ -1,19 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Compute syllable and phoneme counts based on the CMU Pronouciation data.
-
-Methods:
-* get_all_MNphonemes: get the dict of (word, mean number of phonemes)
-* is_int: test if a string represents an integer
-* count_syllables: count the number of syllables in a list of phonemes
-* get_MNsyllables: get the mean number of syllables for a word
-* get_all_MNsyllables: get the dict of (word, mean number of syllables)
-
-Variables:
-* prondict: the CMU pronounciation dict. Used internally.
-
-"""
+"""Compute syllable and phoneme counts based on the CMU Pronouciation data."""
 
 
 from __future__ import division
@@ -24,26 +12,30 @@ from nltk.corpus import cmudict
 
 from util.generic import is_int
 
-prondict = cmudict.dict()
+_prondict = cmudict.dict()
 
 
 def get_all_MNphonemes():
-    """Get the dict of (word, mean number of phonemes)."""
+    """Get the dict of `(word, mean number of phonemes)`."""
+
     return dict((w, array([len(pron) for pron in prons]).mean())
-                for w, prons in prondict.iteritems())
+                for w, prons in _prondict.iteritems())
 
 
 def count_syllables(pron):
-    """Count the number of syllables in a list of phonemes."""
+    """Count the number of syllables in `pron`, a list of phonemes."""
+
     return sum([is_int(ph[-1]) for ph in pron])
 
 
 def get_MNsyllables(w):
-    """Get the mean number of syllables for a word."""
-    Nsyllabless = array([count_syllables(pron) for pron in prondict[w]])
+    """Get the mean number of syllables for a word `w`."""
+
+    Nsyllabless = array([count_syllables(pron) for pron in _prondict[w]])
     return Nsyllabless.mean()
 
 
 def get_all_MNsyllables():
-    """Get the dict of (word, mean number of syllables)."""
-    return dict((w, get_MNsyllables(w)) for w in prondict.iterkeys())
+    """Get the dict of `(word, mean number of syllables)`."""
+
+    return dict((w, get_MNsyllables(w)) for w in _prondict.iterkeys())
