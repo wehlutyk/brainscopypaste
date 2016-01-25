c07e0038bd873f21da13ad71fc47d7b331d21f6f
Sébastien Lerique <sl@mehho.net>
2016-01-25 14:09:25 +0100

    Refactor loading, use session.bulk_save_objects to optimize

--- brainscopypaste/load.py
+++ brainscopypaste/load.py
@@ -30,18 +30,14 @@ class MemeTrackerParser:
         self.parsed = False
 
         # Keep track of current cluster and quote.
-        self.cluster = None
-        self.cluster_size = None
-        self.cluster_frequency = None
-        self.quote = None
-        self.quote_size = None
-        self.quote_frequency = None
+        self._cluster = None
+        self._quote = None
 
-    def skip_header(self, f):
-        """Skip the header lines in an open file."""
+    def _skip_header(self):
+        """Skip the header lines in the open file."""
 
         for i in range(self.header_size):
-            f.readline()
+            self._file.readline()
 
     def parse(self):
         """Parse using the defined cluster-, quote-, and url-handlers."""
@@ -53,116 +49,146 @@ class MemeTrackerParser:
             raise ValueError('Parser has already run')
 
         lines_left = self.line_count - self.header_size
-        with open(self.filename, 'rb', encoding='utf-8') as infile, \
-                ProgressBar(max_value=lines_left, redirect_stdout=True) as bar:
-
-            # The first lines are not data.
-            self.skip_header(infile)
-
-            cluster_line = infile.readline()
-            clusters_read = 1
-            lines_read = 1
-            bar.update(lines_read)
-
-            while cluster_line is not None:
-
-                with session_scope() as session:
-
-                    # Start this cluster
-                    line0, fields = self.parse_line(cluster_line)
-                    if line0[0] == '':
-                        raise ValueError(
-                                ("Our supposed cluster_line ('{}', line {}) "
-                                 "is not a cluster line!")
-                                .format(cluster_line,
-                                        lines_read + self.header_size))
-                    self.handle_cluster(fields)
-                    session.add(self.cluster)
-                    cluster_line = None
-
-                    # And keep reading until the next one, or exhaustion
-                    for line in infile:
-                        lines_read += 1
-                        bar.update(lines_read)
-
-                        line0, fields = self.parse_line(line)
-                        if line0[0] != '':
-                            # This is a cluster definition line.
-
-                            # Stop if asked to
-                            if (self.limit is not None and
-                                    clusters_read >= self.limit):
-                                break
-
-                            # Check the cluster and quote we just finished
-                            if self.cluster is not None:
-                                self.check_cluster()
-                            if self.quote is not None:
-                                self.check_quote()
-
-                            # And move to next one
-                            self.cluster = None
-                            self.quote = None
-                            cluster_line = line
-                            clusters_read += 1
-                            break
-                        elif line[0] == '\t' and line[1] != '\t':
-                            # This is a quote definition line.
-                            # Check the quote we just finished
-                            if self.quote is not None:
-                                self.check_quote()
-                            # And move to next one
-                            self.handle_quote(fields)
-                        elif (line[0] == '\t' and line[1] == '\t' and
-                                line[2] != '\t'):
-                            # This is a url definition line.
-                            self.handle_url(fields)
-
-        # Check final cluster and quote
-        with session_scope() as session:
-            self.cluster = session.merge(self.cluster)
-            self.check_cluster()
-            self.quote = session.merge(self.quote)
-            self.check_quote()
+        with open(self.filename, 'rb', encoding='utf-8') as self._file, \
+                ProgressBar(max_value=lines_left,
+                            redirect_stdout=True) as self._bar:
+            self._parse()
 
         # Don't do this twice.
         self.parsed = True
         click.secho('OK', fg='green', bold=True)
 
-    def parse_line(self, line):
+    def _parse(self):
+
+        # The first lines are not data.
+        self._skip_header()
+
+        # Initialize the parsing with the first line.
+        self._cluster_line = self._file.readline()
+        self._clusters_seen = 1
+        self._lines_read = 1
+        self._bar.update(self._lines_read)
+
+        while self._cluster_line is not None:
+            self._parse_cluster_block()
+
+    def _parse_cluster_block(self):
+        # Check we have a cluster line and parse it.
+        tipe, fields = self._parse_line(self._cluster_line)
+        # If self._cluster_line stays None, _parse() stops.
+        # So it's filled further down when we get to the next cluster
+        # definition line (unless self.limit says we should read
+        # only a subset of all clusters).
+        self._cluster_line = None
+        if tipe != 'cluster':
+            raise ValueError(
+                ("Our supposed cluster_line ('{}', line {}) "
+                 "is not a cluster line!").format(
+                     self._cluster_line, self._lines_read + self.header_size))
+
+        # Create the cluster.
+        self._objects = []
+        self._checks = {'cluster': {}, 'quotes': {}}
+        self._handle_cluster(fields)
+
+        # Keep reading until the next one, or exhaustion.
+        for line in self._file:
+            self._lines_read += 1
+            self._bar.update(self._lines_read)
+
+            tipe, fields = self._parse_line(line)
+            if tipe == 'cluster':
+                # Feed a new cluster_line to keep going, unless asked to stop.
+                if self.limit is None or self._clusters_seen < self.limit:
+                    self._cluster_line = line
+                    self._clusters_seen += 1
+                    # We can clean up the current quote straight away.
+                    self._quote = None
+                break
+            elif tipe == 'quote':
+                self._handle_quote(fields)
+            elif tipe == 'url':
+                self._handle_url(fields)
+
+        # Save everything.
+        with session_scope() as session:
+            session.bulk_save_objects(self._objects)
+
+        # Check everything.
+        self._check_cluster_block()
+
+        # Clean up.
+        self._cluster = None
+        self._checks = None
+        self._objects = None
+
+    def _check_cluster_block(self):
+        with session_scope() as session:
+            cluster = session.query(Cluster).get(self._cluster.id)
+
+            # Check the cluster itself.
+            check = self._checks['cluster'][cluster.id]
+            err_end = (' #{} does not match value'
+                       ' in file').format(cluster.sid)
+            if check['size'] != cluster.size:
+                raise ValueError("Cluster size" + err_end)
+            if check['frequency'] != cluster.frequency:
+                raise ValueError("Cluster frequency" + err_end)
+
+            # Check each quote.
+            for quote in cluster.quotes:
+                check = self._checks['quotes'][quote.id]
+                err_end = ' #{} does not match value in file'.format(quote.sid)
+                if check['size'] != quote.size:
+                    raise ValueError("Quote size" + err_end)
+                if check['frequency'] != quote.frequency:
+                    raise ValueError("Quote frequency" + err_end)
+
+    @classmethod
+    def _parse_line(self, line):
         line0 = re.split(r'[\xa0\s+\t\r\n]+', line)
-        return line0, re.split(r'[\t\r\n]', line)
+        if line0[0] != '':
+            tipe = 'cluster'
+        elif line[0] == '\t' and line[1] != '\t':
+            tipe = 'quote'
+        elif line[0] == '\t' and line[1] == '\t' and line[2] != '\t':
+            tipe = 'url'
+        else:
+            tipe = None
+        return tipe, re.split(r'[\t\r\n]', line)
 
-    def check_cluster(self):
-        err_end = (' #{} does not match value'
-                   ' in file').format(self.cluster.sid)
-        if self.cluster_size != self.cluster.size:
-            raise ValueError("Cluster size" + err_end)
-        if self.cluster_frequency != self.cluster.frequency:
-            raise ValueError("Cluster frequency" + err_end)
+    def _handle_cluster(self, fields):
+        id = int(fields[3])
+        self._cluster = Cluster(id=id, sid=id, source='memetracker')
+        self._objects.append(self._cluster)
 
-    def handle_cluster(self, line_fields):
-        self.cluster = Cluster(sid=int(line_fields[3]), source='memetracker')
-        self.cluster_size = int(line_fields[0])
-        self.cluster_frequency = int(line_fields[1])
+        # Save checks for later on.
+        cluster_size = int(fields[0])
+        cluster_frequency = int(fields[1])
+        self._checks['cluster'][self._cluster.id] = {
+            'size': cluster_size,
+            'frequency': cluster_frequency
+        }
 
-    def check_quote(self):
-        err_end = ' #{} does not match value in file'.format(self.quote.sid)
-        if self.quote_size != self.quote.size:
-            raise ValueError("Quote size" + err_end)
-        if self.quote_frequency != self.quote.frequency:
-            raise ValueError("Quote frequency" + err_end)
+    def _handle_quote(self, fields):
+        id = int(fields[4])
+        self._quote = Quote(cluster_id=self._cluster.id,
+                            id=id, sid=id, string=fields[3])
+        self._objects.append(self._quote)
 
-    def handle_quote(self, line_fields):
-        self.quote = Quote(sid=int(line_fields[4]), string=line_fields[3])
-        self.quote_size = int(line_fields[2])
-        self.quote_frequency = int(line_fields[1])
-        self.cluster.quotes.append(self.quote)
+        # Save checks for later on.
+        quote_size = int(fields[2])
+        quote_frequency = int(fields[1])
+        self._checks['quotes'][self._quote.id] = {
+            'size': quote_size,
+            'frequency': quote_frequency
+        }
 
-    def handle_url(self, line_fields):
-        timestamp = datetime.strptime(line_fields[2], '%Y-%m-%d %H:%M:%S')
+    def _handle_url(self, fields):
+        timestamp = datetime.strptime(fields[2], '%Y-%m-%d %H:%M:%S')
         assert timestamp.tzinfo is None
 
-        url = Url(timestamp=timestamp, frequency=int(line_fields[3]),
-                  url_type=line_fields[4], url=line_fields[5])
-        self.quote.urls.append(url)
+        url = Url(quote_id=self._quote.id,
+                  timestamp=timestamp, frequency=int(fields[3]),
+                  url_type=fields[4], url=fields[5])
+        self._objects.append(url)
