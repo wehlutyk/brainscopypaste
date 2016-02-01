0eaeb116a8cb8a54ddfab760818dbb07c6540de8
Sébastien Lerique <sl@mehho.net>
2016-02-01 12:29:04 +0100

    Stub mining classes

---
+++ brainscopypaste/mine.py
@@ -0,0 +1,24 @@
+class Model:
+    # time: continuous/discrete
+    # source: all/majority
+    # past: all/last bin
+    # destination: all/not in past
+    pass
+
+
+class Miner:
+
+    # iterate candidate substitutions
+    pass
+
+
+class SourceValidator:
+    pass
+
+
+class DestinationValidator:
+    pass
+
+
+class ValidatorMixin:
+    pass
