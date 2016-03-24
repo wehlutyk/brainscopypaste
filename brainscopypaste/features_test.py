fc5486366fe39f15d032f8e544939c2d6d4b33e2
Sébastien Lerique <sl@mehho.net>
2016-03-24 13:41:45 +0100

    fex/features: use pytest.mark.skipif for test needing computed files

--- brainscopypaste/features_test.py
+++ brainscopypaste/features_test.py
@@ -1,5 +1,4 @@
 from os.path import exists
-from warnings import warn
 
 import pytest
 import numpy as np
@@ -86,59 +85,50 @@ def test_aoa():
     assert np.isnan(SubstitutionFeaturesMixin._aoa('wickiup'))
 
 
-def guard_feature_test(file):
-    if not exists(file):
-        # TODO: make this how up in pytest report
-        warn("Skipping feature test: feature file ('{}') not "
-             'found. You should run `brainscopypaste load features` '
-             'beforehand if you want this test to run.'
-             .format(file))
-        return False
-    else:
-        return True
-
-
+@pytest.mark.skipif(not exists(fa_norms_degrees_pickle),
+                    reason='missing computed feature')
 def test_fa_degree():
-    if guard_feature_test(fa_norms_degrees_pickle):
-        assert SubstitutionFeaturesMixin._fa_degree('abdomen') == \
-            1 / (10617 - 1)
-        assert SubstitutionFeaturesMixin._fa_degree('speaker') == \
-            9 / (10617 - 1)
-        assert np.isnan(SubstitutionFeaturesMixin._fa_degree('wickiup'))
+    assert SubstitutionFeaturesMixin._fa_degree('abdomen') == 1 / (10617 - 1)
+    assert SubstitutionFeaturesMixin._fa_degree('speaker') == 9 / (10617 - 1)
+    assert np.isnan(SubstitutionFeaturesMixin._fa_degree('wickiup'))
 
 
+@pytest.mark.skipif(not exists(fa_norms_PR_scores_pickle),
+                    reason='missing computed feature')
 def test_fa_pagerank():
-    if guard_feature_test(fa_norms_PR_scores_pickle):
-        assert abs(SubstitutionFeaturesMixin._fa_pagerank('you') -
-                   0.0006390798677378056) < 1e-15
-        assert abs(SubstitutionFeaturesMixin._fa_pagerank('play') -
-                   0.0012008124120435305) < 1e-15
-        assert np.isnan(SubstitutionFeaturesMixin._fa_pagerank('wickiup'))
+    assert abs(SubstitutionFeaturesMixin._fa_pagerank('you') -
+               0.0006390798677378056) < 1e-15
+    assert abs(SubstitutionFeaturesMixin._fa_pagerank('play') -
+               0.0012008124120435305) < 1e-15
+    assert np.isnan(SubstitutionFeaturesMixin._fa_pagerank('wickiup'))
 
 
+@pytest.mark.skipif(not exists(fa_norms_BCs_pickle),
+                    reason='missing computed feature')
 def test_fa_betweenness():
-    if guard_feature_test(fa_norms_BCs_pickle):
-        assert SubstitutionFeaturesMixin.\
-            _fa_betweenness('dog') == 0.0046938277117769605
-        assert SubstitutionFeaturesMixin.\
-            _fa_betweenness('play') == 0.008277234906313704
-        assert np.isnan(SubstitutionFeaturesMixin._fa_betweenness('wickiup'))
+    assert SubstitutionFeaturesMixin.\
+        _fa_betweenness('dog') == 0.0046938277117769605
+    assert SubstitutionFeaturesMixin.\
+        _fa_betweenness('play') == 0.008277234906313704
+    assert np.isnan(SubstitutionFeaturesMixin._fa_betweenness('wickiup'))
 
 
+@pytest.mark.skipif(not exists(fa_norms_CCs_pickle),
+                    reason='missing computed feature')
 def test_fa_clustering():
-    if guard_feature_test(fa_norms_CCs_pickle):
-        assert SubstitutionFeaturesMixin.\
-            _fa_clustering('dog') == 0.0009318641757868838
-        assert SubstitutionFeaturesMixin.\
-            _fa_clustering('play') == 0.0016238663632016216
-        assert np.isnan(SubstitutionFeaturesMixin._fa_clustering('wickiup'))
+    assert SubstitutionFeaturesMixin.\
+        _fa_clustering('dog') == 0.0009318641757868838
+    assert SubstitutionFeaturesMixin.\
+        _fa_clustering('play') == 0.0016238663632016216
+    assert np.isnan(SubstitutionFeaturesMixin._fa_clustering('wickiup'))
 
 
+@pytest.mark.skipif(not exists(mt_frequencies_pickle),
+                    reason='missing computed feature')
 def test_frequency():
-    if guard_feature_test(mt_frequencies_pickle):
-        assert SubstitutionFeaturesMixin._frequency('dog') == 7865
-        assert SubstitutionFeaturesMixin._frequency('play') == 45848
-        assert np.isnan(SubstitutionFeaturesMixin._frequency('wickiup'))
+    assert SubstitutionFeaturesMixin._frequency('dog') == 7865
+    assert SubstitutionFeaturesMixin._frequency('play') == 45848
+    assert np.isnan(SubstitutionFeaturesMixin._frequency('wickiup'))
 
 
 def test_phonological_density():
@@ -152,10 +142,17 @@ def test_orthographical_density():
                     _orthographical_density('wickiup'))
 
 
-def test_features():
+@pytest.fixture
+def normal_substitution():
     q1 = Quote(string='It is the containing part')
     q2 = Quote(string='It is the other part')
-    s = Substitution(source=q1, destination=q2, start=0, position=3)
+    return Substitution(source=q1, destination=q2, start=0, position=3)
+
+
+def test_features(normal_substitution):
+    # A shortcut.
+    s = normal_substitution
+
     # Check we defined the right substitution.
     assert s.tokens == ('containing', 'other')
     assert s.lemmas == ('contain', 'other')
@@ -185,52 +182,16 @@ def test_features():
     assert s.features('orthographical_density',
                       sentence_relative=True) == (0 - 62/5, 5 - 67/5)
 
-    # Synonyms count, age-of-acquisition, FA features, and frequency
-    # are right, and computed on lemmas.
+    # Synonyms count and age-of-acquisition are right, and computed on lemmas.
+    # (the rest of the features need computed files,
+    # and are tested separately).
     assert s.features('synonyms_count') == (3, .5)
     assert s.features('aoa') == (7.88, 5.33)
-    if guard_feature_test(fa_norms_degrees_pickle):
-        assert s.features('fa_degree') == \
-            (9.419743782969103e-05, 0.0008477769404672192)
-    if guard_feature_test(fa_norms_PR_scores_pickle):
-        assert abs(s.features('fa_pagerank')[0] -
-                   2.9236183726513393e-05) < 1e-15
-        assert abs(s.features('fa_pagerank')[1] -
-                   6.421655879054584e-05) < 1e-15
-    if guard_feature_test(fa_norms_BCs_pickle):
-        assert np.isnan(s.features('fa_betweenness')[0])
-        assert s.features('fa_betweenness')[1] == 0.0003369277738594168
-    if guard_feature_test(fa_norms_CCs_pickle):
-        assert np.isnan(s.features('fa_clustering')[0])
-        assert s.features('fa_clustering')[1] == 0.0037154495910700605
-    if guard_feature_test(mt_frequencies_pickle):
-        assert s.features('frequency') == (3992, 81603)
     # Same with features computed relative to sentence.
     assert s.features('synonyms_count', sentence_relative=True) == \
         (3 - 1.8611111111111112, .5 - 1.2361111111111112)
     assert s.features('aoa', sentence_relative=True) == \
         (7.88 - 6.033333333333334, 5.33 - 5.183333333333334)
-    if guard_feature_test(fa_norms_degrees_pickle):
-        assert s.features('fa_degree', sentence_relative=True) == \
-            (9.419743782969103e-05 - 0.0010550113036925397,
-             0.0008477769404672192 - 0.0012057272042200451)
-    if guard_feature_test(fa_norms_PR_scores_pickle):
-        assert abs(s.features('fa_pagerank', sentence_relative=True)[0] -
-                   (2.9236183726513393e-05 - 9.2820794154173557e-05)) < 1e-15
-        assert abs(s.features('fa_pagerank', sentence_relative=True)[1] -
-                   (6.421655879054584e-05 - 9.9816869166980042e-05)) < 1e-15
-    if guard_feature_test(fa_norms_BCs_pickle):
-        assert np.isnan(s.features('fa_betweenness',
-                                   sentence_relative=True)[0])
-        assert s.features('fa_betweenness', sentence_relative=True)[1] == \
-            0.0003369277738594168 - 0.00081995401378403285
-    if guard_feature_test(fa_norms_CCs_pickle):
-        assert np.isnan(s.features('fa_clustering', sentence_relative=True)[0])
-        assert s.features('fa_clustering', sentence_relative=True)[1] == \
-            0.0037154495910700605 - 0.0021628891370054143
-    if guard_feature_test(mt_frequencies_pickle):
-        assert s.features('frequency', sentence_relative=True) == \
-            (3992 - 1373885.6000000001, 81603 - 1389407.8)
 
     # Unknown words are ignored. Also when in the rest of the sentence.
     q1 = Quote(string='makakiki is the goal')
@@ -241,3 +202,70 @@ def test_features():
     assert np.isnan(s.features('syllables_count')[1])
     assert s.features('syllables_count', sentence_relative=True)[0] == 1 - 3/3
     assert np.isnan(s.features('syllables_count', sentence_relative=True)[1])
+
+
+@pytest.mark.skipif(not exists(fa_norms_degrees_pickle),
+                    reason='missing computed feature')
+def test_features_fa_degree(normal_substitution):
+    s = normal_substitution
+    # Values are right, and computed on lemmas.
+    assert s.features('fa_degree') == \
+        (9.419743782969103e-05, 0.0008477769404672192)
+    # Same with features computed relative to sentence.
+    assert s.features('fa_degree', sentence_relative=True) == \
+        (9.419743782969103e-05 - 0.0010550113036925397,
+         0.0008477769404672192 - 0.0012057272042200451)
+
+
+@pytest.mark.skipif(not exists(fa_norms_PR_scores_pickle),
+                    reason='missing computed feature')
+def test_features_fa_pagerank(normal_substitution):
+    s = normal_substitution
+    # Values are right, and computed on lemmas.
+    assert abs(s.features('fa_pagerank')[0] -
+               2.9236183726513393e-05) < 1e-15
+    assert abs(s.features('fa_pagerank')[1] -
+               6.421655879054584e-05) < 1e-15
+    # Same with features computed relative to sentence.
+    assert abs(s.features('fa_pagerank', sentence_relative=True)[0] -
+               (2.9236183726513393e-05 - 9.2820794154173557e-05)) < 1e-15
+    assert abs(s.features('fa_pagerank', sentence_relative=True)[1] -
+               (6.421655879054584e-05 - 9.9816869166980042e-05)) < 1e-15
+
+
+@pytest.mark.skipif(not exists(fa_norms_BCs_pickle),
+                    reason='missing computed feature')
+def test_features_fa_betweenness(normal_substitution):
+    s = normal_substitution
+    # Values are right, and computed on lemmas.
+    assert np.isnan(s.features('fa_betweenness')[0])
+    assert s.features('fa_betweenness')[1] == 0.0003369277738594168
+    # Same with features computed relative to sentence.
+    assert np.isnan(s.features('fa_betweenness',
+                               sentence_relative=True)[0])
+    assert s.features('fa_betweenness', sentence_relative=True)[1] == \
+        0.0003369277738594168 - 0.00081995401378403285
+
+
+@pytest.mark.skipif(not exists(fa_norms_CCs_pickle),
+                    reason='missing computed feature')
+def test_features_fa_clustering(normal_substitution):
+    s = normal_substitution
+    # Values are right, and computed on lemmas.
+    assert np.isnan(s.features('fa_clustering')[0])
+    assert s.features('fa_clustering')[1] == 0.0037154495910700605
+    # Same with features computed relative to sentence.
+    assert np.isnan(s.features('fa_clustering', sentence_relative=True)[0])
+    assert s.features('fa_clustering', sentence_relative=True)[1] == \
+        0.0037154495910700605 - 0.0021628891370054143
+
+
+@pytest.mark.skipif(not exists(mt_frequencies_pickle),
+                    reason='missing computed feature')
+def test_features_frequency(normal_substitution):
+    s = normal_substitution
+    # Values are right, and computed on lemmas.
+    assert s.features('frequency') == (3992, 81603)
+    # Same with features computed relative to sentence.
+    assert s.features('frequency', sentence_relative=True) == \
+        (3992 - 1373885.6000000001, 81603 - 1389407.8)
