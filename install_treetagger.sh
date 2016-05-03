178cb02edb10707b383843b3bdd12b275b7477af
Sébastien Lerique <sl@mehho.net>
2016-05-03 17:01:48 +0200

    fix/treetagger: update treetagger download links

--- install_treetagger.sh
+++ install_treetagger.sh
@@ -11,11 +11,11 @@ cd treetagger
 # Download necessary zips
 echo "[BrainsCopyPaste installer] Downloading executables and parameter files..."
 echo
-wget ftp://ftp.ims.uni-stuttgart.de/pub/corpora/tree-tagger-linux-3.2.tar.gz
-wget ftp://ftp.ims.uni-stuttgart.de/pub/corpora/tagger-scripts.tar.gz
-wget ftp://ftp.ims.uni-stuttgart.de/pub/corpora/install-tagger.sh
-wget ftp://ftp.ims.uni-stuttgart.de/pub/corpora/english-par-linux-3.2.bin.gz
-wget ftp://ftp.ims.uni-stuttgart.de/pub/corpora/english-chunker-par-linux-3.2.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tree-tagger-linux-3.2.tar.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/tagger-scripts.tar.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/install-tagger.sh
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-par-linux-3.2-utf8.bin.gz
+wget http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/english-chunker-par-linux-3.2-utf8.bin.gz
 
 # Run the installer
 echo "[BrainsCopyPaste installer] Installing files..."
