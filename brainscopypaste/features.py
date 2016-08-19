29c4a18390eb698debf2b30e8d1d53ac74e9f1c2
Sébastien Lerique <sl@mehho.net>
2016-08-19 17:15:23 +0200

    docs/reference: document part of features.py

--- brainscopypaste/features.py
+++ brainscopypaste/features.py
@@ -1,5 +1,12 @@
 """Features for words in substitutions.
 
+This module defines the :class:`SubstitutionFeaturesMixin` which is used to
+augment :class:`~.db.Substitution`\ s with convenience methods that give access
+to feature values and related computed values (e.g. sentence-relative feature
+values and values for composite features).
+
+A few other utility functions that load data for the features are also defined.
+
 """
 
 
@@ -20,12 +27,31 @@ logger = logging.getLogger(__name__)
 
 @memoized
 def _get_pronunciations():
+    """Get the CMU pronunciation data as a dict.
+
+    Returns
+    -------
+    dict
+        Association of words to their list of possible pronunciations.
+
+    """
+
     logger.debug('Loading CMU data')
     return cmudict.dict()
 
 
 @memoized
 def _get_aoa():
+    """Get the Age-of-Acquisition data as a dict.
+
+    Returns
+    -------
+    dict
+        Association of words to their average age of acquisition. `NA` values
+        in the originating data set are ignored.
+
+    """
+
     logger.debug('Loading Age-of-Acquisition data')
 
     aoa = {}
@@ -45,6 +71,19 @@ def _get_aoa():
 
 @memoized
 def _get_clearpond():
+    """Get CLEARPOND neighbourhood density data as a dict.
+
+    Returns
+    -------
+    dict
+        `Dict` with two keys: `orthographic` and `phonological`. `orthographic`
+        contains a dict associating words to their orthographic neighbourhood
+        density (CLEARPOND's `OTAN` column). `phonological` contains a dict
+        associating words to their phonological neighbourhood density
+        (CLEARPOND's `PTAN` column).
+
+    """
+
     logger.debug('Loading Clearpond data')
 
     clearpond_orthographic = {}
@@ -67,6 +106,47 @@ def _get_clearpond():
 
 class SubstitutionFeaturesMixin:
 
+    """Mixin for :class:`~.db.Substitution`\ s adding feature-related
+    functionality.
+
+    Methods in this class fall into 3 categories:
+
+    * Raw feature methods: they are :class:`~.utils.memoized` class methods of
+      the form `cls._feature_name(cls, word=None)`. Calling them with a `word`
+      returns either the feature value of that word, or `np.nan` if the word is
+      not encoded. Calling them with `word=None` returns the set of words
+      encoded by that feature (which is used to compute e.g. averages over the
+      pool of words encoded by that feature). Their docstring (which you will
+      see below if you're reading this in a web browser) is the short name used
+      to identify e.g. the feature's column in analyses in notebooks. These
+      methods are used internally by the class, to provide the next category of
+      methods.
+    * Useful feature methods that can be used in analyses: :meth:`features`,
+      :meth:`feature_average`, :meth:`source_destination_features`,
+      :meth:`components`, and :meth:`component_average`. These methods use the
+      raw feature methods (previous category) and the utility methods (next
+      category) to compute feature or composite values (eventually relative to
+      sentence) on the source or destination words or sentences.
+    * Private utility methods: :meth:`_component`,
+      :meth:`_source_destination_components`, :meth:`_average`,
+      :meth:`_static_average`, :meth:`_strict_synonyms`,
+      :meth:`_substitution_features`, and :meth:`_transformed_feature`. These
+      methods are used by the previous category of methods.
+
+    Read the source of the first category (raw features) to know how exactly an
+    individual feature is computed. Read the docstrings (and source) of the
+    second category (useful methods for analyses) to learn how to use this
+    class in analyses. Read the docstrings (and source) of the third category
+    (private utility methods) to learn how the whole class assembles its
+    different parts together.
+
+    """
+
+    #: Association of available features to `(source_type, transform)` tuples:
+    #: `source_type` defines if a feature is computed on tokens or lemmas, and
+    #: `transform` defines how a feature value is transformed (for now either
+    #: identity or log) because of the shape of its distribution (see the
+    #: `notebook/feature_distributions.ipynb` notebook for more details).
     __features__ = {
         # feature_name:           (source_type, transform)
         'syllables_count':        ('tokens', lambda x: x),
@@ -80,7 +160,7 @@ class SubstitutionFeaturesMixin:
         'clustering':             ('lemmas', np.log),
         'frequency':              ('lemmas', np.log),
         'phonological_density':   ('tokens', np.log),
-        'orthographic_density': ('tokens', np.log),
+        'orthographic_density':   ('tokens', np.log),
     }
 
     @memoized
@@ -229,6 +309,42 @@ class SubstitutionFeaturesMixin:
     @memoized
     def feature_average(self, name, source_synonyms=False,
                         sentence_relative=None):
+        """Compute the average of feature `name` over all coded words.
+
+        If `source_synonyms` is `True`, the method computes the average feature
+        of the synonyms of the source word of this substitution.  Otherwise, it
+        computes the average over all words coded by the feature.
+
+        If `sentence_relative` is not `None`, it is used to aggregate feature
+        values of the words in the source sentence of this substitution, and
+        this method returns the feature average minus that aggregate value.
+        For instance, if `sentence_relative=np.median`, `feature_average`
+        returns the average feature minus the median feature value in the
+        sentence (words in the sentence valued at `np.nan` are ignored).
+
+        Parameters
+        ----------
+        name : str
+            Name of the feature for which to compute an average.
+        source_synonyms : bool, optional
+            If `True`, compute the average feature of the synonyms of the
+            source word in this substitution. If `False` (default), compute the
+            average over all coded words.
+        sentence_relative : aggregating function, optional
+            If not `None` (which is the default), return average feature
+            relative to feature values of the sentence aggregated by this
+            function.
+
+        Returns
+        -------
+        float
+            Average feature, of all coded words or of synonyms of the
+            substitution's source word (depending on `source_synonyms`),
+            relative to an aggregated sentence value if `sentence_relative`
+            specifies it.
+
+        """
+
         tfeature = self._transformed_feature(name)
         avg = self._average(tfeature, source_synonyms)
 
@@ -245,6 +361,49 @@ class SubstitutionFeaturesMixin:
     @memoized
     def component_average(self, n, pca, feature_names,
                           source_synonyms=False, sentence_relative=None):
+        """Compute the average, over all coded words, of component `n` of `pca`
+        using `feature_names`.
+
+        If `source_synonyms` is `True`, the method computes the average
+        component of the synonyms of the source word of this substitution.
+        Otherwise, it computes the average over all words coded by the
+        component.
+
+        If `sentence_relative` is not `None`, it is used to aggregate component
+        values of the words in the source sentence of this substitution, and
+        this method returns the component average minus that aggregate value.
+        For instance, if `sentence_relative=np.median`, `component_average`
+        returns the average component minus the median component value in the
+        sentence (words in the sentence valued at `np.nan` are ignored).
+
+        Parameters
+        ----------
+        n : int
+            Index of the component in `pca` that is to be computed.
+        pca : :class:`sklearn.decomposition.PCA`
+            :class:`~sklearn.decomposition.PCA` instance that was computed
+            using the features listed in `feature_names`.
+        feature_names : tuple of str
+            Tuple of feature names used in the computation of `pca`.
+        source_synonyms : bool, optional
+            If `True`, compute the average component of the synonyms of the
+            source word in this substitution. If `False` (default), compute the
+            average over all coded words.
+        sentence_relative : aggregating function, optional
+            If not `None` (which is the default), return average component
+            relative to component values of the sentence aggregated by this
+            function.
+
+        Returns
+        -------
+        float
+            Average component, of all coded words or of synonyms of the
+            substitution's source word (depending on `source_synonyms`),
+            relative to an aggregated sentence value if `sentence_relative`
+            specifies it.
+
+        """
+
         component = self._component(n, pca, feature_names)
         avg = self._average(component, source_synonyms)
 
@@ -262,6 +421,46 @@ class SubstitutionFeaturesMixin:
     @classmethod
     @memoized
     def _component(cls, n, pca, feature_names):
+        """Get a function computing component `n` of `pca` using `feature_names`.
+
+        Parameters
+        ----------
+        n : int
+            Index of the component in `pca` that is to be computed.
+        pca : :class:`sklearn.decomposition.PCA`
+            :class:`~sklearn.decomposition.PCA` instance that was computed
+            using the features listed in `feature_names`.
+        feature_names : tuple of str
+            Tuple of feature names used in the computation of `pca`.
+
+        Returns
+        -------
+        component : function
+            The component function, with signature `component(word=None)`. Call
+            `component()` to get the set of words encoded by that component
+            (which is the set of words encoded by all features in
+            `feature_names`). Call `component(word)` to get the component value
+            of `word` (or `np.nan` if `word` is not coded by that component).
+
+        Examples
+        --------
+        Get the first component of "dog" in a PCA with very few words, using
+        features `aoa`, `frequency`, and `letters_count`:
+
+        >>> mixin = SubstitutionFeaturesMixin()
+        >>> feature_names = ('aoa', 'frequency', 'letters_count')
+        >>> features = list(map(mixin._transformed_feature,
+        ...                     feature_names))
+        >>> values = np.array([[f(w) for f in features]
+        ...                    for w in ['bird', 'cat', 'human']])
+        >>> from sklearn.decomposition import PCA
+        >>> pca = PCA(n_components=2)
+        >>> pca.fit(values)
+        >>> mixin._component(0, pca, feature_names)('dog')
+        -0.14284518091970733
+
+        """
+
         # Check the PCA was computed for as many features as we're given.
         n_features = len(feature_names)
         assert n_features == len(pca.mean_)
@@ -282,6 +481,10 @@ class SubstitutionFeaturesMixin:
             words.update(tfeature())
 
         def transform(word_tfeatures):
+            """Get component `n` of `pca` based on a list of transformed word
+            feature values; returns `np.nan` if some feature values are
+            `np.nan`."""
+
             return pca.transform(word_tfeatures.reshape(1, -1))[0, n]\
                 if np.isfinite(word_tfeatures).all() else np.nan
 
@@ -293,6 +496,7 @@ class SubstitutionFeaturesMixin:
                                           dtype=np.float_)
                 return transform(word_tfeatures)
 
+        # Set the right docstring and name on the component function.
         component.__name__ = '_component_{}'.format(n)
         component.__doc__ = 'component {}'.format(n)
 
@@ -301,6 +505,48 @@ class SubstitutionFeaturesMixin:
     @classmethod
     @memoized
     def _transformed_feature(cls, name):
+        """Get a function computing feature `name`, transformed as defined by
+        :attr:`__features__`.
+
+        Some features have a very skewed distribution (e.g. exponential, where
+        a few words are valued orders of magnitude more than the vast majority
+        of words), so we use their log-transformed values in the analysis to
+        make them comparable to more regular features. The :attr:`__features__`
+        attribute (which appears in the source code but not in the web version
+        of these docs) defines which features are transformed how. Given a
+        feature `name`, this method will generate a function that proxies calls
+        to the raw feature method, and transforms the value if necessary.
+
+        This method is :meth:`~.utils.memoized` for speed, since other methods
+        call it all the time.
+
+        Parameters
+        ----------
+        name : str
+            Name of the feature for which to create a function, without
+            preceding underscore; for instance, call
+            `cls._transformed_feature('aoa')` to get a function that uses the
+            :meth:`_aoa` class method.
+
+        Returns
+        -------
+        feature : function
+            The feature function, with signature `feature(word=None)`. Call
+            `feature()` to get the set of words encoded by that feature. Call
+            `feature(word)` to get the transformed feature value of `word` (or
+            `np.nan` if `word` is not coded by that feature).
+
+        Examples
+        --------
+        Get the transformed frequency value of "dog":
+
+        >>> mixin = SubstitutionFeaturesMixin()
+        >>> logfrequency = mixin._transformed_feature('frequency')
+        >>> logfrequency('dog') == np.log(mixin._frequency('dog'))
+        True
+
+        """
+
         if name not in cls.__features__:
             raise ValueError("Unknown feature: '{}'".format(name))
         _feature = getattr(cls, '_' + name)
@@ -312,6 +558,7 @@ class SubstitutionFeaturesMixin:
             else:
                 return transform(_feature(word))
 
+        # Set the right docstring and name on the transformed feature function.
         functools.update_wrapper(feature, _feature)
         if transform is np.log:
             feature.__name__ = '_log' + feature.__name__
@@ -321,6 +568,9 @@ class SubstitutionFeaturesMixin:
 
     @classmethod
     def _strict_synonyms(cls, word):
+        """Get the set of synonyms of `word` through WordNet, excluding `word`
+        itself; empty if nothing is found."""
+
         # wordnet.synsets() lemmatizes words, so we might as well control it.
         # This also lets us check the lemma is present in the generated
         # synonym list further down.
